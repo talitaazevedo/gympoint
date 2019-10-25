3c83a48d52fbbb90ddb49c0554274665cce45683
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-24 22:25:35 -0300

    terceiro passo migration corrigida

--- src/database/migrations/20191025004426-create_students.js
+++ src/database/migrations/20191025004426-create_students.js
@@ -1,10 +1,48 @@
-//Passo 03 => criando Migrations que são tabelas
+// Passo 03 => criando Migrations que são tabelas
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        return queryInterface.createTable('students', {
+            // Definindo objetos da tabela
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            nome: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            idade: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            peso: {
+                type: Sequelize.DECIMAL,
+                allowNull: false,
+            },
+            altura: {
+                type: Sequelize.DECIMAL,
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
     },
 
     down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('users');
+        return queryInterface.dropTable('students');
     },
 };
