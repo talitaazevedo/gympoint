0aef042e392897d710500793fe7c44aba5209ddb
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-26 21:52:04 -0300

    Criaçao de Rotas do projeto

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/students', StudentController.store);
 
 export default routes;
