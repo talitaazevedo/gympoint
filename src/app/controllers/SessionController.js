bc4fb4cb465ce0d15dff9daf1a58b2a718807456
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-26 22:08:12 -0300

    alterando script de autenticaçao com jwt

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,11 +1,21 @@
 import jwt from 'jsonwebtoken';
-
+import * as Yup from 'yup';
 import User from '../models/User';
 
-import auth from '../../config/auth';
+import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
+        // initializing schema from data validation
+        const schema = Yup.object().shape({
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string().required(),
+        });
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation Fails' });
+        }
         const { email, password } = req.body;
 
         const user = await User.findOne({ where: { email } });
@@ -15,20 +25,17 @@ class SessionController {
         if (!(await user.checkPassword(password))) {
             return res.status(401).json({ error: 'Password does not match' });
         }
-        const { id, name, password_hash } = user;
-        return res.json(
-            {
+        const { id, name } = user;
+        return res.json({
+            user: {
                 id,
                 name,
                 email,
-                password_hash,
             },
-            {
-                token: jwt.sign({ id }, auth.secret, {
-                    expiresIn: auth.expiresIn,
-                }),
-            }
-        );
+            token: jwt.sign({ id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+            }),
+        });
     }
 }
 export default new SessionController();
